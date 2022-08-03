36de73289187af5194a57e84f660586958c340e0
unknown <example@gmail.com>
2022-08-03 09:09:58 +0500

    first commit

---
+++ lib/Home/home_page.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 840),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            elevation: 0.0,
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: 10,
+                height: 10,
+              ),
+              //
+              Column(
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    child: Image.asset(
+                      "assets/image/group.png",
+                      width: 282.w,
+                      height: 110.h,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(left: 70.w),
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 130.w,
+                              height: 2.h,
+                              color: Colors.black,
+                            ),
+                            SizedBox(height: 4.h),
+                            Container(
+                              width: 130.w,
+                              height: 2.h,
+                              color: Colors.black,
+                            ),
+                            SizedBox(height: 4.h),
+                            Container(
+                              width: 130.w,
+                              height: 2.h,
+                              color: Colors.black,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(left: 5.w),
+                        child: Text(
+                          "CAR RENTAL",
+                          style: TextStyle(
+                              fontSize: 20.sp, fontWeight: FontWeight.w700),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+              //
+              Column(
+                children: [
+                  GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      width: 335.w,
+                      height: 54.h,
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(8.r),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Sign in",
+                          style: TextStyle(
+                              fontSize: 17.sp,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10.h),
+                  Container(
+                    margin: EdgeInsets.only(bottom: 30.h),
+                    width: 335.w,
+                    height: 54.h,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      border: Border.all(width: 1, color: Colors.black),
+                      borderRadius: BorderRadius.circular(8.r),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Sign up",
+                        style: TextStyle(
+                          fontSize: 17.sp,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
